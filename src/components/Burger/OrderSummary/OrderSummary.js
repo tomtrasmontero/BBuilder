46e46864c20ce47f75791d0ce124a66b563ad7e0
tomtrasmontero <tomtrasmontero@gmail.com>
2018-01-24 20:04:26 -0500

    added mobile first css and added sidedrawer menu

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary =(props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  // could be a functional component
+  componentWillUpdate() {
+    console.log('ordersummary Update');
+  }
+
+  render(){
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map((igKey, idx) => {
       return (
         <li key={igKey + idx}>
           <span
-          style={{textTransform: 'capitalize'}}
-          >{igKey}
-          </span>: {props.ingredients[igKey]}
+            style={{textTransform: 'capitalize'}}
+            >{igKey}
+          </span>: {this.props.ingredients[igKey]}
         </li>
       )
     })
-
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout</p>
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary;
+
+export default OrderSummary;
